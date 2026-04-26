d9ee8a654f0e9beb83b85fc28c20f7b03b3880cd
Smile <90579042+smile0101@users.noreply.github.com>
2026-04-26 17:13:46 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- stock.py
+++ stock.py
@@ -118,10 +118,12 @@ def fetch_supply_data(stock_name, stock_code, excel_df_json):
     MONGO_URL  = st.secrets["mongo_uri"]
     try:
         with MongoClient(
-           MONGO_URL,
-          serverSelectionTimeoutMS=5000,
-          tlsCAFile=certifi.where()
-          ) as client:
+            MONGO_URL,
+            serverSelectionTimeoutMS=5000,
+            tls=True,
+            tlsCAFile=certifi.where(),
+            ssl_cert_reqs='CERT_NONE'
+        ) as client:
             col   = client.forin.stocks
             db_df = pd.DataFrame(col.find({"종목명": stock_name}, {"_id": 0}))
     except Exception as e:
